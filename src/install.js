657281d6c60e03aaafeeb874b241314b6959aee8
Ronnie Rap <1546947+ronnierap@users.noreply.github.com>
2024-05-07 13:04:53 +0200

    await imports

--- src/install.js
+++ src/install.js
@@ -1,15 +1,15 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-(function () {
+(async function() {
 	var osd = window.OpenSeadragon;
 	var plugin = window.OsdSelectionPlugin;
 	if (!osd) {
-		osd = import('openseadragon');
+		osd = await import('openseadragon');
 		if (!osd) {
 			throw new Error('OpenSeadragon is missing.');
 		}
 	}
 	if (!plugin) {
-		plugin = import('./selection');
+		plugin = await import('./selection');
 		if (!plugin) {
 			throw new Error(
 				'OsdSelectionPlugin in missing',
